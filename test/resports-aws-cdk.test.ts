62acfcfa6d754a108192ac7a26982bf08fa11511
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-11 15:10:32 +1030

    Initial commit

---
+++ test/resports-aws-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as ResportsAwsCdk from '../lib/resports-aws-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/resports-aws-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new ResportsAwsCdk.ResportsAwsCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
