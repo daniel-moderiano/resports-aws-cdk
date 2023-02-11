8228cd9def618ba3fc2ffe560b18395873ef74fa
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-11 17:38:36 +1030

    Create new lambda function resource definition for our hello lambda function

--- lib/resports-aws-cdk-stack.ts
+++ lib/resports-aws-cdk-stack.ts
@@ -1,8 +1,24 @@
 import * as cdk from "aws-cdk-lib";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import {
+  NodejsFunction,
+  NodejsFunctionProps,
+} from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
+import { join } from "path";
 
 export class ResportsAwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+
+    const nodeJsFunctionProps: NodejsFunctionProps = {
+      runtime: lambda.Runtime.NODEJS_16_X, // execution environment
+    };
+
+    // defines an AWS Lambda resource
+    const hello = new NodejsFunction(this, "HelloHandler", {
+      entry: join(__dirname, "lambdas", "hello.ts"),
+      ...nodeJsFunctionProps,
+    });
   }
 }
