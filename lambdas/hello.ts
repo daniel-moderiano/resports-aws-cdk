b5c262b3c3f2dcb3a3f4d1cab259a588744d28eb
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-12 19:15:10 +1030

    Modify lambda function to act differently depending on route and request method

--- lambdas/hello.ts
+++ lambdas/hello.ts
@@ -1,10 +1,14 @@
-import { APIGatewayProxyEventV2, Handler } from "aws-lambda";
+import { APIGatewayProxyEventV2, Context, Handler } from "aws-lambda";
 
 export const handler: Handler = async function (event: APIGatewayProxyEventV2) {
   console.log("request:", JSON.stringify(event, undefined, 2));
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
-    body: `Hello ${event.rawPath}`,
+    body: `
+      Hello, you've hit ${event.rawPath}!\n
+      The params are ${JSON.stringify(event.pathParameters)}.\n
+      The method is ${event.routeKey.split(" ")[0]}
+    `,
   };
 };
