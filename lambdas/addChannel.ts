6528b8c7f0e781a21c237df538b95e8f36ee0cb5
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-12 20:35:12 +1030

    Fix hanlder function messages to reflect plural vs singular action

--- lambdas/addChannel.ts
+++ lambdas/addChannel.ts
@@ -4,6 +4,6 @@ export const handler: Handler = async function () {
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
-    body: "Add channels",
+    body: "Add channel",
   };
 };
--- lambdas/getChannel.ts
+++ lambdas/getChannel.ts
@@ -4,6 +4,6 @@ export const handler: Handler = async function () {
   return {
     statusCode: 200,
     headers: { "Content-Type": "text/plain" },
-    body: "Get channels",
+    body: "Get channel",
   };
 };
