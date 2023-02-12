17b56d9b28107169e0ba9bfb739ed21f6b0de3de
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-12 18:48:46 +1030

    API Gateway (#2)
    * Add lambda rest api resource
    
    * Adjust lambda handler event to a more specific type
    
    * Install alpha http API aws-cdk packages to create a new httpApi
    
    * Add lambda integration with HTTP API for hello function
    
    * Add /hello route and connect to hello function integration
    
    * Add CFN output to display the API URL when deploying
    
    * Adjust lambda typings to match new payload version

--- lib/resports-aws-cdk-stack.ts
+++ lib/resports-aws-cdk-stack.ts
@@ -1,11 +1,13 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import { HttpApi, HttpMethod } from "@aws-cdk/aws-apigatewayv2-alpha";
 import {
   NodejsFunction,
   NodejsFunctionProps,
 } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
 import { join } from "path";
+import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
 
 export class ResportsAwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -19,6 +21,26 @@ export class ResportsAwsCdkStack extends cdk.Stack {
     const hello = new NodejsFunction(this, "HelloHandler", {
       entry: join(__dirname, "/../lambdas", "hello.ts"),
       ...nodeJsFunctionProps,
+    });
+
+    // Integrate our hello function with the HTTP API. This connects the API route to the lambda service
+    const helloIntegration = new HttpLambdaIntegration(
+      "HelloIntegration",
+      hello
+    );
+
+    // defines an API Gateway HTTP API resource
+    const httpApi = new HttpApi(this, "HttpApi");
+
+    // Attaches our integrated function to the "/hello" path
+    httpApi.addRoutes({
+      path: "/hello",
+      methods: [HttpMethod.GET],
+      integration: helloIntegration,
+    });
+
+    new cdk.CfnOutput(this, "apiUrl", {
+      value: httpApi.url ? httpApi.url : "No URL found",
     });
   }
 }
