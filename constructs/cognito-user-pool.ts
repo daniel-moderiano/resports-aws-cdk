35ca9b5a38ac0e2b50c831e95aebac80866b0869
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-14 11:37:17 +1030

    Further customise the Cognito user pool settings

--- constructs/cognito-user-pool.ts
+++ constructs/cognito-user-pool.ts
@@ -8,6 +8,30 @@ export class CognitoUserPool extends Construct {
     const userPool = new cognito.UserPool(this, "TestUserPool", {
       userPoolName: "test-user-pool",
       signInCaseSensitive: false, // case insensitive is preferred in most situations
+
+      // Allow self sign up, else users could only be signed up by an administrator
+      selfSignUpEnabled: true,
+
+      // Allow users to sign in with either username or email
+      signInAliases: {
+        username: true,
+        email: true,
+      },
+
+      // Users must provide full name on sign-up, but cannot change it thereafter
+      standardAttributes: {
+        fullname: {
+          required: true,
+          mutable: false,
+        },
+      },
+
+      // Users can opt-in to MFA, but only TOTP apps are avaiable as an authentication method
+      mfa: cognito.Mfa.OPTIONAL,
+      mfaSecondFactor: {
+        sms: false,
+        otp: true,
+      },
     });
   }
 }
