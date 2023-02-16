2db0ae68b1499e1f12b218f600de33c4d3a3660e
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-16 10:45:22 +1030

    Remove entire Cognito construct, planning to move in favour of client side auth0 implementation with KWT API gateway authoriser

--- lib/resports-aws-cdk-stack.ts
+++ lib/resports-aws-cdk-stack.ts
@@ -1,12 +1,6 @@
 import * as cdk from "aws-cdk-lib";
 import * as lambda from "aws-cdk-lib/aws-lambda";
-import {
-  HttpApi,
-  HttpMethod,
-  HttpAuthorizer,
-  HttpAuthorizerType,
-  IHttpAuthorizer,
-} from "@aws-cdk/aws-apigatewayv2-alpha";
+import { HttpApi, HttpMethod } from "@aws-cdk/aws-apigatewayv2-alpha";
 import {
   NodejsFunction,
   NodejsFunctionProps,
@@ -14,18 +8,10 @@ import {
 import { Construct } from "constructs";
 import { join } from "path";
 import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
-import { CognitoUserPool } from "../constructs/cognito-user-pool";
-import {
-  HttpLambdaAuthorizer,
-  HttpUserPoolAuthorizer,
-} from "@aws-cdk/aws-apigatewayv2-authorizers-alpha";
 
 export class ResportsAwsCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // Instantiate the separately defined cognito construct
-    const cognitoUserPool = new CognitoUserPool(this, "CognitoUserPool");
 
     const nodeJsFunctionProps: NodejsFunctionProps = {
       runtime: lambda.Runtime.NODEJS_16_X, // execution environment
@@ -98,22 +84,11 @@ export class ResportsAwsCdkStack extends cdk.Stack {
     // defines an API Gateway HTTP API resource
     const httpApi = new HttpApi(this, "HttpApi");
 
-    // Create a general Authorizer for handling secure API requests, that can later be attached to certain routes
-    const authorizer = new HttpUserPoolAuthorizer(
-      "HttpAuthorizer",
-      cognitoUserPool.userPool,
-      {
-        userPoolClients: [cognitoUserPool.userPoolClient],
-        identitySource: ["$request.header.Authorization"],
-      }
-    );
-
     // Define the REST channel routes
     httpApi.addRoutes({
       path: "/channels",
       methods: [HttpMethod.GET],
       integration: getAllChannelsIntegration,
-      authorizer,
     });
 
     httpApi.addRoutes({
