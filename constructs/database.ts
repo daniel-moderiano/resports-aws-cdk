8992d1bb87091afd40f6f603b6d45148a9a3437b
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-22 13:08:49 +1030

    Define initial databae parameters for later use

--- constructs/database.ts
+++ constructs/database.ts
@@ -1,8 +1,21 @@
 import { Construct } from "constructs";
-import { DatabaseInstance } from "aws-cdk-lib/aws-rds";
+import {
+  DatabaseInstance,
+  DatabaseInstanceEngine,
+  PostgresEngineVersion,
+} from "aws-cdk-lib/aws-rds";
+import { InstanceClass, InstanceSize, InstanceType } from "aws-cdk-lib/aws-ec2";
 
 export class PostgresDatabase extends Construct {
   constructor(scope: Construct, id: string) {
     super(scope, id);
+
+    // Define required database parameters
+    const engine = DatabaseInstanceEngine.postgres({
+      version: PostgresEngineVersion.VER_13_7,
+    });
+    const instanceType = InstanceType.of(InstanceClass.T3, InstanceSize.MICRO);
+    const port = 5432;
+    const databaseName = "resports-dev";
   }
 }
