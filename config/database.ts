90198c860905fe97b576dd51ef4d2638123f1927
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-22 17:35:49 +1030

    Write test handler for interaction with RDS

--- config/database.ts
+++ config/database.ts
@@ -2,7 +2,8 @@ require("dotenv").config();
 import { cleanEnv, str } from "envalid";
 
 export const env = cleanEnv(process.env, {
-  DATABASE_USE: str(),
+  DATABASE_USER: str(),
   DATABASE_PASSWORD: str(),
   DATABASE_NAME: str(),
+  DATABASE_HOST: str(),
 });
--- lambdas/private.ts
+++ lambdas/private.ts
@@ -1,9 +1,23 @@
 import { Handler, APIGatewayProxyEventV2 } from "aws-lambda";
+import { Client } from "pg";
+import { env } from "../config/database";
 
 export const handler: Handler = async function (event: APIGatewayProxyEventV2) {
-  return {
-    statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: "You've activated the private handler",
-  };
+  try {
+    const client = new Client({
+      user: env.DATABASE_USER,
+      host: env.DATABASE_HOST,
+      database: env.DATABASE_NAME,
+      password: env.DATABASE_PASSWORD,
+      port: 5432,
+    });
+    await client.connect();
+    const res = await client.query("SELECT $1::text as message", [
+      "Hello world!",
+    ]);
+    console.log(res.rows[0].message); // Hello world!
+    await client.end();
+  } catch (err) {
+    console.log("error while trying to connect to db");
+  }
 };
