8c852cf2df21498be1ad4f2a08cc3036528f9adf
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-22 15:48:23 +1030

    Fully define the initial RDS instance

--- constructs/database.ts
+++ constructs/database.ts
@@ -4,18 +4,41 @@ import {
   DatabaseInstanceEngine,
   PostgresEngineVersion,
 } from "aws-cdk-lib/aws-rds";
-import { InstanceClass, InstanceSize, InstanceType } from "aws-cdk-lib/aws-ec2";
+import {
+  InstanceClass,
+  InstanceSize,
+  InstanceType,
+  SecurityGroup,
+  SubnetType,
+  Vpc,
+} from "aws-cdk-lib/aws-ec2";
+import { Duration } from "aws-cdk-lib";
+
+interface DatabaseProps {
+  vpc: Vpc;
+  securityGroup?: SecurityGroup;
+}
 
 export class PostgresDatabase extends Construct {
-  constructor(scope: Construct, id: string) {
+  public database: DatabaseInstance;
+
+  constructor(scope: Construct, id: string, props: DatabaseProps) {
     super(scope, id);
 
-    // Define required database parameters
-    const engine = DatabaseInstanceEngine.postgres({
-      version: PostgresEngineVersion.VER_13_7,
+    const { vpc } = props;
+
+    this.database = new DatabaseInstance(this, "ResportsDatabase", {
+      engine: DatabaseInstanceEngine.postgres({
+        version: PostgresEngineVersion.VER_13_7,
+      }),
+      vpc,
+      vpcSubnets: { subnetType: SubnetType.PRIVATE_ISOLATED },
+      instanceType: InstanceType.of(InstanceClass.T3, InstanceSize.MICRO),
+      port: 5432,
+      databaseName: "resports-dev",
+      backupRetention: Duration.days(0),
     });
-    const instanceType = InstanceType.of(InstanceClass.T3, InstanceSize.MICRO);
-    const port = 5432;
-    const databaseName = "resports-dev";
+
+    this.database.connections.allowInternally;
   }
 }
