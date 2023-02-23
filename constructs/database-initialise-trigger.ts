61112e56f838468a8488fa7e2f2c7c620428e028
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-23 23:01:11 +1030

    Define a simple lambda that should trigger on deploy

---
+++ constructs/database-initialise-trigger.ts
@@ -0,0 +1,38 @@
+import { Construct } from "constructs";
+import { Duration, triggers } from "aws-cdk-lib";
+import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
+import { join } from "path";
+import * as lambda from "aws-cdk-lib/aws-lambda";
+import { SubnetType, Vpc } from "aws-cdk-lib/aws-ec2";
+import { env } from "../config/database";
+
+interface InitialiserProps {
+  databaseContstruct: Construct;
+  vpc: Vpc;
+}
+
+export class DatabaseInitialiseTrigger extends Construct {
+  constructor(scope: Construct, id: string, props: InitialiserProps) {
+    super(scope, id);
+
+    const upsertUser = new NodejsFunction(this, "DatabaseInitialiser", {
+      entry: join(__dirname, "/../lambdas", "initialiseDatabase.ts"),
+      vpc: props.vpc,
+      vpcSubnets: { subnetType: SubnetType.PRIVATE_ISOLATED },
+      environment: {
+        DATABASE_HOST: env.DATABASE_HOST,
+        DATABASE_NAME: env.DATABASE_NAME,
+        DATABASE_PASSWORD: env.DATABASE_PASSWORD,
+        DATABASE_USER: env.DATABASE_USER,
+      },
+      timeout: Duration.seconds(30),
+      runtime: lambda.Runtime.NODEJS_16_X,
+    });
+
+    const trigger = new triggers.Trigger(this, "DatabaseInitialiseTrigger", {
+      handler: upsertUser,
+      executeAfter: [props.databaseContstruct],
+      executeOnHandlerChange: false,
+    });
+  }
+}
---
+++ lambdas/initialiseDatabase.ts
@@ -0,0 +1,35 @@
+import { Handler, APIGatewayProxyEventV2 } from "aws-lambda";
+import { Client } from "pg";
+import { env } from "../config/database";
+
+export const handler: Handler = async function (event: APIGatewayProxyEventV2) {
+  try {
+    const client = new Client({
+      user: env.DATABASE_USER,
+      host: env.DATABASE_HOST,
+      database: env.DATABASE_NAME,
+      password: env.DATABASE_PASSWORD,
+      port: 5432,
+    });
+    await client.connect();
+    const res = await client.query("SELECT $1::text as message", [
+      "Hello world!",
+    ]);
+    console.log(res.rows[0].message); // Hello world!
+    await client.end();
+
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "text/plain" },
+      body: res.rows[0].message,
+    };
+  } catch (err) {
+    console.log(err);
+
+    return {
+      statusCode: 500,
+      headers: { "Content-Type": "text/plain" },
+      body: "Welp, something went wrong",
+    };
+  }
+};
