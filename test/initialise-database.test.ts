6817419c6531690b0521befba734e882d65a3e20
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-24 14:44:10 +1030

    Add tests for all database initialising-related queries

--- test/initialise-database.test.ts
+++ test/initialise-database.test.ts
@@ -1,15 +1,33 @@
 import { testPool } from "../config/database";
-import { dropExistingTables } from "../helpers/initdb";
+import { createNewTables, dropExistingTables } from "../helpers/initdb";
+
+// Reset the database to baseline empty tables
+beforeAll(async () => {
+  await dropExistingTables(testPool);
+  await createNewTables(testPool);
+});
 
 afterAll(async () => {
   await testPool.end();
 });
 
-it("returns JSON response", async () => {
-  await dropExistingTables(testPool);
-  const response = await testPool.query(
-    "SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'"
-  );
+// Returns all user created tables
+const queryForAllTables =
+  "SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'";
 
-  expect(response.rowCount).toBe(0);
+it("resets/initialises correctly with 3 new tables", async () => {
+  const result = await testPool.query(queryForAllTables);
+  expect(result.rowCount).toBe(3);
+});
+
+it("correctly removes all tables", async () => {
+  await dropExistingTables(testPool);
+  const result = await testPool.query(queryForAllTables);
+  expect(result.rowCount).toBe(0);
+});
+
+it("correctly recreates all tables", async () => {
+  await createNewTables(testPool);
+  const result = await testPool.query(queryForAllTables);
+  expect(result.rowCount).toBe(3);
 });
