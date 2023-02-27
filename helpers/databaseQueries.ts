f4d79a8c81122d74c704cf55e629c1da7b670719
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-02-27 21:54:24 +1030

    Create all saved_channels table queries

--- helpers/databaseQueries.ts
+++ helpers/databaseQueries.ts
@@ -20,13 +20,56 @@ export const deleteChannel = async (database: Database, channelId: string) => {
   ]);
 };
 
-export const deleteUser = async (database: Database, userId: string) => {
-  return database.query("DELETE FROM users WHERE user_id=$1", [userId]);
-};
-
 export const upsertUser = async (database: Database, user: User) => {
   return database.query(
     "INSERT INTO users (user_id, email, email_verified) VALUES ($1, $2, $3) ON CONFLICT (user_id) DO UPDATE SET email_verified = $3",
     [user.user_id, user.email, user.email_verified]
+  );
+};
+
+export const deleteUser = async (database: Database, userId: string) => {
+  return database.query("DELETE FROM users WHERE user_id=$1", [userId]);
+};
+
+export const selectSavedChannel = async (
+  database: Database,
+  userId: string,
+  channelId: string
+) => {
+  return database.query(
+    "SELECT * FROM saved_channels WHERE user_id=$1 AND channel_id=$2",
+    [userId, channelId]
+  );
+};
+
+export const selectUserSavedChannels = async (
+  database: Database,
+  userId: string
+) => {
+  return database.query(
+    "SELECT channel_id FROM saved_channels WHERE user_id=$1",
+    [userId]
+  );
+};
+
+export const insertSavedChannel = async (
+  database: Database,
+  userId: string,
+  channelId: string
+) => {
+  return database.query(
+    "INSERT INTO saved_channels (user_id, channel_id) VALUES ($1, $2) ON CONFLICT (user_id, channel_id) DO NOTHING",
+    [userId, channelId]
+  );
+};
+
+export const deleteSavedChannel = async (
+  database: Database,
+  userId: string,
+  channelId: string
+) => {
+  return database.query(
+    "DELETE FROM saved_channels WHERE user_id=$1 AND channel_id=$2",
+    [userId, channelId]
   );
 };
