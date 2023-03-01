c8847ae577c7aa7b97bbbcb9db50c54363d30b75
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-03-01 20:35:07 +1030

    Add tests and functionality for delete user handler

--- lambdas/deleteUser.ts
+++ lambdas/deleteUser.ts
@@ -1,9 +1,59 @@
-import { Handler } from "aws-lambda";
+import { APIGatewayProxyEventV2, Handler } from "aws-lambda";
+import { is, object, string } from "superstruct";
+import { deleteUser } from "../helpers/databaseQueries";
+import { Client } from "pg";
+import { databaseConfig } from "../config/database";
 
-export const handler: Handler = async function () {
-  return {
+const UserIdStruct = object({
+  user_id: string(),
+});
+
+export const handler: Handler = async function (event: APIGatewayProxyEventV2) {
+  const userInformation = event.pathParameters;
+
+  if (!userInformation) {
+    return JSON.stringify({
+      statusCode: 400,
+      headers: { "Content-Type": "application/json" },
+      body: {
+        message: "Bad request. Missing user ID.",
+      },
+    });
+  }
+
+  if (!is(userInformation, UserIdStruct)) {
+    return JSON.stringify({
+      statusCode: 400,
+      headers: { "Content-Type": "application/json" },
+      body: {
+        message: "Bad request. Invalid user ID.",
+      },
+    });
+  }
+
+  const client = new Client({
+    user: databaseConfig.DATABASE_USER,
+    host: databaseConfig.DATABASE_HOST,
+    database: databaseConfig.DATABASE_NAME,
+    password: databaseConfig.DATABASE_PASSWORD,
+    port: 5432,
+  });
+
+  // The following code will throw a generic 500 internal server error. We might consider a try/catch, but I don't think we would handle the error any differently
+  await client.connect();
+
+  const result = await deleteUser(client, userInformation.user_id);
+
+  await client.end();
+
+  return JSON.stringify({
     statusCode: 200,
-    headers: { "Content-Type": "text/plain" },
-    body: "Delete user",
-  };
+    headers: { "Content-Type": "application/json" },
+    body: {
+      message:
+        result.rowCount === 0
+          ? "No existing user to delete"
+          : `User ${userInformation.user_id} deleted.`,
+    },
+  });
 };
