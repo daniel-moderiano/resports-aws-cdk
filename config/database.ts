31adead4c26be98ff1db66ebb80c407befabd2c5
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-03-01 19:14:58 +1030

    Move the db test pool definition into the only file using it

--- config/database.ts
+++ config/database.ts
@@ -1,4 +1,3 @@
-import { Pool } from "pg";
 import "dotenv/config";
 import { cleanEnv, str } from "envalid";
 
@@ -15,9 +14,3 @@ export const databaseConfig = {
   DATABASE_NAME: env.DATABASE_NAME,
   DATABASE_HOST: env.DATABASE_HOST,
 };
-
-// Uses an in-memory Postgres database for testing
-export const testPool = new Pool({
-  // DATABASE_URL will be automatically set during test setup by @databases/pg-test
-  connectionString: process.env.DATABASE_URL,
-});
