4ba383305bbe76315b6353ae0d939c789101e640
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-07-12 19:55:06 +0930

    Turn off virtuals in schema object conversions

--- src/models/schemas.ts
+++ src/models/schemas.ts
@@ -1,17 +1,29 @@
 import { ChannelDocument, UserDocument } from "@/types";
 import mongoose, { Schema } from "mongoose";
 
-const ChannelSchema = new Schema({
-  _id: { type: String, required: true }, // This will be the YouTube or Twitch ID
-  platform: { type: String, enum: ["youtube", "twitch"], required: true },
-});
+const ChannelSchema = new Schema(
+  {
+    _id: { type: String, required: true }, // This will be the YouTube or Twitch ID
+    platform: { type: String, enum: ["youtube", "twitch"], required: true },
+  },
+  {
+    toObject: { virtuals: false },
+    toJSON: { virtuals: false },
+  }
+);
 
-const UserSchema = new Schema({
-  _id: { type: String, required: true }, // This will be the Auth0 ID
-  email: { type: String, required: true, unique: true },
-  email_verified: { type: Boolean, required: true },
-  saved_channels: [{ type: String, ref: "Channel" }], // An array of references to Channel documents
-});
+const UserSchema = new Schema(
+  {
+    _id: { type: String, required: true }, // This will be the Auth0 ID
+    email: { type: String, required: true, unique: true },
+    email_verified: { type: Boolean, required: true },
+    saved_channels: [{ type: String, ref: "Channel" }], // An array of references to Channel documents
+  },
+  {
+    toObject: { virtuals: false },
+    toJSON: { virtuals: false },
+  }
+);
 
 export const ChannelModel = mongoose.model<ChannelDocument>(
   "Channel",
