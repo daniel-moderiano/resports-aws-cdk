fa05131796a55b24e4e3190ce55b04a7777fe9c0
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-07-12 19:29:25 +0930

    Remove logging during DB connection

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -2,13 +2,8 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   try {
-    // MongoDB connection string
     const mongoUri: string = process.env.MONGO_URI || "";
-
-    // Connect to MongoDB
-    const conn = await mongoose.connect(mongoUri);
-
-    console.log(`MongoDB connected: ${conn.connection.host}`);
+    await mongoose.connect(mongoUri);
   } catch (error) {
     if (error instanceof Error) {
       // type guard
