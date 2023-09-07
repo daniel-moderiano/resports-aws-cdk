44c35123ceb1fcc919b266e888a44da318a225df
Daniel Moderiano <59184832+daniel-moderiano@users.noreply.github.com>
2023-09-07 17:41:42 +0930

    Adapt tests to handle removal of path params

--- src/test/addSavedChannel.test.ts
+++ src/test/addSavedChannel.test.ts
@@ -2,20 +2,8 @@ import { APIGatewayProxyEventV2 } from "aws-lambda";
 import { handler } from "@/lambdas/addSavedChannel";
 import { mockCallback, mockContext, mockEvent } from "./constants";
 
-const eventNoParams: APIGatewayProxyEventV2 = {
-  ...mockEvent,
-  pathParameters: undefined,
-  body: JSON.stringify({
-    userId: "1234",
-    platform: "twitch",
-  }),
-};
-
 const eventNoBody: APIGatewayProxyEventV2 = {
   ...mockEvent,
-  pathParameters: {
-    userId: "1234",
-  },
   body: undefined,
 };
 
@@ -38,19 +26,6 @@ it("returns bad request for missing body", async () => {
       headers: { "Content-Type": "application/json" },
       body: {
         errorMessage: "Channel data is missing.",
-      },
-    })
-  );
-});
-
-it("returns bad request for missing URL params", async () => {
-  const response = await handler(eventNoParams, mockContext, mockCallback);
-  expect(response).toBe(
-    JSON.stringify({
-      statusCode: 400,
-      headers: { "Content-Type": "application/json" },
-      body: {
-        errorMessage: "User ID is missing.",
       },
     })
   );
